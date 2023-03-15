ef9e723ed8f1e4946e1088f17d828f7fc3992612
mabdullah12101 <muhabdullah12101@gmail.com>
2023-03-15 14:40:00 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_packages_library/hello.dart';
+
+void main() {
+  print(sayHello("Doel"));
+
+  var customer = Customer();
+  var product = Product();
+  var categoty =
+}
